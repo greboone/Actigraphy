7bb67a96996600a988530b382751f1ff403d7a40
Greboone <guto.ebone3553@gmail.com>
2019-11-23 10:04:03 -0300

    Criação dos arquivos a serem desenvolvidos

--- main.R
+++ main.R
@@ -1,10 +1,13 @@
+#include
+source(file = "src/csv_to_agd.R")
+
+
  # ideia de menu
 while(1){
   i=as.numeric(readline(prompt="Number of file: "))
   if(i == 0){
     break;
   }else if(i==1){
-    print("Oi")
+    ola()
   }
 }
-  
---
+++ src/csv_to_agd.R
@@ -0,0 +1,3 @@
+ola <- function(){
+    print("OLA")
+}
