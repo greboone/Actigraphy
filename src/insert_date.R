e8a75d5bcfe716888c771aedfc7bf66ef0cc5705
Greboone <guto.ebone3553@gmail.com>
2019-12-02 17:50:18 -0300

    a

--- src/insert_date.R
+++ src/insert_date.R
@@ -1,22 +1,34 @@
 insert_date <- function(data){
-  obtain_timestamp <- function(data){
-    data$timestamp <- as.character(difftime(data$timestamp,
-                                            "0001-01-01 00:00:00", tz = "UTC", units = c("secs"))*1e7)
-    data
-  }
-  
+  library('lubridate')
   lines = tail(data$line,1)
   i=1
   day = 1
-  while(i <=lines){
+  
+  b = data.frame(timestamp = data$time)
+  class(b$timestamp) = "posix"
+  
+  while(i <= lines){
     tmp = data$dayofweek[i] 
-    tmp1 = data$dayofweek[i+1]
+    if(i>=lines-1){
+      tmp1 = tmp
+    }else{
+      tmp1 = data$dayofweek[i+1]
+    }
     date <- paste0("0001-01-0", day)
-    data$time[i] <- sub("",date,data$time[i])
+    aux1 <- paste0("*",data$time[i],"*")
     
+    aux <- paste(date, data$time[i])
+    #print(aux)
+    b$timestamp <- as.POSIXct(aux, format = "%Y-%m-%d %H:%M:%S")
+    print(b$timestamp[i])
+  
+    #print(data$time[i])
+    #data$time[i] <- sub("",date,data$time[i])
+    #print(date$time[i])
     if(tmp != tmp1){
       day = day+1
     }
-  }
+    i = i+1
+   }
   data
 }
